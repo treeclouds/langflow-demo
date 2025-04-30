c0f0cef64224fac5cb36263f4a14d1ae848c7092
Nikolas Wijaya <nwijaya@treeclouds.com>
2025-04-30 17:10:31 +0700

    remove default chat

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import "./App.css";
 import { Admin, Home, SummarizePDF, UserChat } from "./pages";
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  const chatRef = useRef(null);
+  // const chatRef = useRef(null);
 
-  useEffect(() => {
-    if (chatRef.current) {
-      chatRef.current.setAttribute("window_title", "NIX Customer Service");
-      chatRef.current.setAttribute(
-        "flow_id",
-        "cb8db5f4-f987-4d99-88fc-0b9bae8bfd7e"
-      );
-      chatRef.current.setAttribute("host_url", "https://langflow.bawana.com");
+  // useEffect(() => {
+  //   if (chatRef.current) {
+  //     chatRef.current.setAttribute("window_title", "NIX Customer Service");
+  //     chatRef.current.setAttribute(
+  //       "flow_id",
+  //       "cb8db5f4-f987-4d99-88fc-0b9bae8bfd7e"
+  //     );
+  //     chatRef.current.setAttribute("host_url", "https://langflow.bawana.com");
 
-      const apiKey = process.env.REACT_APP_LANGFLOW_API_KEY;
-      if (apiKey) {
-        chatRef.current.setAttribute("api_key", apiKey);
-      } else {
-        console.warn("Langflow API Key is missing!");
-      }
-    }
-  }, []);
+  //     const apiKey = process.env.REACT_APP_LANGFLOW_API_KEY;
+  //     if (apiKey) {
+  //       chatRef.current.setAttribute("api_key", apiKey);
+  //     } else {
+  //       console.warn("Langflow API Key is missing!");
+  //     }
+  //   }
+  // }, []);
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <langflow-chat ref={chatRef}></langflow-chat>
-      </div>
+      </div> */}
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
